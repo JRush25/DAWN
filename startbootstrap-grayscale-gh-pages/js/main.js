1db7d517f3f768f68a86812149ec692b5f62d56b
Joselyne Isabel Torres Duque <joselynetorres25@hotmail.com>
2020-12-04 02:55:52 -0500

    Navegacion guiada

---
+++ startbootstrap-grayscale-gh-pages/js/main.js
@@ -0,0 +1,30 @@
+const intro = introJs();
+intro.setOptions({
+    steps: [
+        {
+            element:'#step1',
+            intro: 'Bienvenido a este tour por la página'
+        },
+        {
+            element:'#step2',
+            intro: 'Aquí encontrarás las últimas noticias de videojuegos'
+        },
+        {
+            element:'#step3',
+            intro: 'Aquí podrás ver un amplio catálogo de videojuegos'
+        },
+        {
+            element: '#step4',
+            intro: 'Aquí puedes buscar el videojuego de tu preferencia'
+        },
+        {
+            element:'#step5',
+            intro: 'Información sobre el equipo desarrollador'
+        },
+        {
+            element: '#step6',
+            intro: 'Envíanos tus mensajes o suegerencias'
+        }
+    ]
+})
+intro.start();
